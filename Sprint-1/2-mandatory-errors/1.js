8fb539ad48a8dea7c1c104f25f5eabeb051ce4ff
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-15 16:47:36 +0200

    const problem variable

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,8 @@
 
 const age = 33;
 age = age + 1;
+
+// this error occur because we are trying to reassign a value to a constant variable
+// to solve this problem we can change the const to let
+// let age = 33;
+// age = age + 1;
