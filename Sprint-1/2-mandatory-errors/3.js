d41a108f0fe6b09cfb898c2a57a6c21cbb4fe4e0
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-15 16:51:22 +0200

    explanation of the slice problem

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,9 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// This error occur because we are trying to use the method slice on a number
+// To solve this problem we can convert the number to a string by using toString() method
+// const last4Digits = cardNumber.toString().slice(-4);
+// console.log(last4Digits);
+// example output: 4213
