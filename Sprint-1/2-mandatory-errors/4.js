29740b9fcc5e834dbc46fb933c998bfa574318fc
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-15 16:52:15 +0200

    problem with start number in a variable

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// This error occur because variable names cannot start with a number
+// To solve this problem we can rename the variables to start with a letter
+// const hour12ClockTime = "20:53";
+// const hour24ClockTime = "08:53";
