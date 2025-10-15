52cbd7c13ac4d0728abfce5b3b8b2b4a1ee92b71
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-15 16:48:30 +0200

    problem of use variable before it's created

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,8 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// this error occur because we are trying to use the variable cityOfBirth before it is declared
+// to solve this problem we can move the declaration of cityOfBirth before the console.log line
+// const cityOfBirth = "Bolton";
+// console.log(`I was born in ${cityOfBirth}`);
