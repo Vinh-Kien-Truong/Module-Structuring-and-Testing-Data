79fb269453e1bd22afa88b727a22ffa53744c0fb
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-23 17:37:52 +0200

    parameter problem

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,5 +1,8 @@
 
 // Predict and explain first BEFORE you run any code...
+/*
+    My prediction is that this code will produce an error because the parameter name is not a valid identifier.
+*/
 
 // this function should square any number but instead we're going to get an error
 
@@ -10,11 +13,21 @@ function square(3) {
 }
 
 // =============> write the error message here
-
+/*
+    SyntaxError: Unexpected number
+*/  
 // =============> explain this error message here
-
+/*
+    The error is occurring because the parameter name "3" is not a valid identifier in JavaScript.
+    In JavaScript, parameter names must start with a letter, underscore (_), or dollar sign ($) and cannot be a number.
+    To fix this problem, we can rename the parameter to a valid identifier, such as "num".
+*/
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
 
+console.log(square(4)); 
