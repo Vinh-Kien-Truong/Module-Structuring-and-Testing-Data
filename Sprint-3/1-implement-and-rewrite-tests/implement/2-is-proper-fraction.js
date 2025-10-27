e7c7e3b85b63b4ea5489508bb0dcfd25827b334b
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-27 01:38:38 +0100

    add test for negative

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -11,6 +11,7 @@ function isProperFraction(numerator, denominator) {
   if (numerator < denominator) {
     return true;
   }
+  return false;
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
@@ -50,6 +51,7 @@ assertEquals(improperFraction, false);
 // Explanation: The fraction -4/7 is a proper fraction because the absolute value of the numerator (4) is less than the denominator (7). The function should return true.
 const negativeFraction = isProperFraction(-4, 7);
 // ====> complete with your assertion
+assertEquals(negativeFraction, true);
 
 // Equal Numerator and Denominator check:
 // Input: numerator = 3, denominator = 3
