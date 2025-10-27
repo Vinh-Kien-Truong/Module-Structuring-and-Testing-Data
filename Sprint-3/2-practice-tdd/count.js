168a5a788f47c55244fbe09950e79d1c21780a95
Truong Vinh Kien <kientruong8543@gmail.com>
2025-10-27 20:39:57 +0100

    implementation count + test mutiple occurence and no occurences

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,10 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  return stringOfCharacters.split("").reduce((count, char) => {
+    if (char === findCharacter) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
 }
 
 module.exports = countChar;
